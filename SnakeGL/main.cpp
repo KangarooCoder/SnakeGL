a736d9143a14e1bc9a70a53e0d237b41308b5005
kangarooCoder <ksbilodeau07@gmail.com>
2020-05-20 14:15:24 -0500

    Initial Commit

---
+++ SnakeGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SnakeGL
+//
+//  Created by Keegan Bilodeau on 5/20/20.
+//  Copyright © 2020 Keegan Bilodeau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
